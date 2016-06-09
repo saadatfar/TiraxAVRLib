5b4fe3bdb5d747441d676099d426f6baef964a03
mh.saadatfar <mh.saadatfar@gmail.com>
2016-06-09 19:28:51 +0430

    Convert C to Lib

--- Include/Sync.h
+++ Include/Sync.h
@@ -1,9 +1,7 @@
-#ifndef  REG_SIZE
-    #error "REG_SIZE" Should be Defined in order to use Tirax Sync     
-#endif
-#ifndef  TRANS_RESEND_TIMER
-    #error "TRANS_RESEND_TIMER" Should be Defined in order to use Tirax Sync     
-#endif
+#ifndef _TIRAX_SYNC_INCLUDED_
+#define _TIRAX_SYNC_INCLUDED_
+
+#define REG_SIZE 64
 
 #define RESEND_T 300
 #define SYW_BUFFER_SIZE 32
@@ -29,26 +27,6 @@
 
 #define REG_BIT_SIZE REG_SIZE/8+(REG_SIZE%8!=0) //Calculate number of bytes needed to store interrupt flags = ceil(REG_SIZE)
 
-unsigned char syReg[REG_SIZE]; //Registers
-unsigned char iFlag[REG_BIT_SIZE]; //Interrupts
-
-//Sync Buffer Variables
-unsigned char syw_wr_index=0,syw_rd_index=0,syw_counter=0;
-bit syw_overflow=0;
-unsigned char syw_buffer[SYW_BUFFER_SIZE];
-
-//TX Buffer Variables
-char tx_buffer[TX_BUFFER_SIZE];
-unsigned char tx_wr_index=0,tx_rd_index=0,tx_counter=0;
-
-unsigned char syw_chk; //Last sent parity byte
-
-//Receiver state machine variables 
-unsigned char read_reg_step=0,read_i;
-unsigned char read_ack_step=0;
-
-void(*intFunc[REG_SIZE])(char); //Interrupt functions variable
-
 void pushSyncBuffer(char i); //Push i th register into sync buffer
 unsigned char readSyncBuffer(); //Read first in sync buffer
 void pullBuffer(); //Pull (delete) first in sync buffer
@@ -66,10 +44,14 @@ void setReadReg(char n,char data); //Send acknowledge and set interrupt
 
 void WriteReg(unsigned char i,unsigned char data);
 
+void RunInterrupts();
+
 #define SetBitReg(i,n) syReg[i]=(1<<n) | syReg[i]  ;pushSyncBuffer(i);
 #define ResetBitReg(i,n) syReg[i]=(0<<n) & syReg[i] ;pushSyncBuffer(i);
 #define ReadBitReg(i,n) (syReg[i] & (1<<n))!=0
 #define ReadReg(i) syReg[i]
 
 
-#include <Sync.c>
+#pragma library Sync.lib
+
+#endif
--- Include/Timer.h
+++ Include/Timer.h
@@ -1,10 +1,12 @@
+#ifndef _TIRAX_TIMER_INCLUDED_
+#define _TIRAX_TIMER_INCLUDED_
+
 #if  _MCU_CLOCK_FREQUENCY_!=8000000
     #error Timer Framework only works at 8 MHZ      
 #endif
 
-#ifndef  TimerN
-    #error "TimerN" Should be Defined in order to use Tirax Timer     
-#endif
+#define TimerN 16
+#define TRANS_RESEND_TIMER 0
 
 #define SETF(i)     tflag[i/8]= tflag[i/8] | (1<<(i%8))
 #define RESETF(i)   tflag[i/8]= tflag[i/8] & (~(1<<(i%8)))
@@ -12,32 +14,13 @@
 
 #define TIMER_BIT_SIZE TimerN/8+(TimerN%8!=0)
 
-unsigned char tflag[TIMER_BIT_SIZE];//Timer Flag Register
-void(*tfunc[TimerN])();// Timer Functions Addresses
-
-unsigned int tt[TimerN];//Timer Time
-unsigned int tj[TimerN];//Job Time
-
-#define StartTimer(i,t,f) tfunc[i]=f;tt[i]=t;tj[i]=0;//i:Timer Number,t:Timer time,f: Function
-#define StartJob(i,t,f) tfunc[i]=f;tt[i]=t;tj[i]=t;
-#define StopTimer(i) tt[i]=0;tj[i]=0;
+void StartTimer(unsigned char i,unsigned int t, void(*f)() );
+void StartJob(unsigned char i,unsigned int t, void(*f)() );
+void StopTimer(unsigned char i);
   
 void RunTimer();
 void initTimer();
 
-interrupt [TIM0_OVF] void timer0_ovf_isr(void)
-{
-    unsigned char i;    
-    TCNT0=0x83; 
-    for (i=0;i<TimerN;i++){
-        if (tt[i]>1){
-            tt[i]--;      
-        }else if(tt[i]==1){
-            tt[i]--;
-            tt[i]=tj[i];
-            SETF(i);
-        }
-    }  
-}
+#pragma library Timer.lib
 
-#include <Timer.c>
+#endif
